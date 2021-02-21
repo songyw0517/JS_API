7b6046705aba4051a4176f79ef9d896bade9f603
songyw0517 <songyw0517@naver.com>
2021-02-22 00:53:19 +0900

    JWT를 이용한 로그인,로그아웃 구현

--- API_Request/js/testAPI.js
+++ API_Request/js/testAPI.js
@@ -42,3 +42,59 @@ function test(){
     console.log(data);
   });
 }
+
+function fetch_form_post(){
+  var id = document.querySelector('#id').value;
+  var pw = document.querySelector('#pw').value;
+  var info = {
+    ID:id,
+    PW:pw
+  };
+  fetch("http://127.0.0.1:5000//form_POST", {
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+    },
+    body:JSON.stringify(info) // str형식으로 보냄
+  })
+  .then((response)=>console.log(typeof(response), response));
+}
+
+function Check_Token(){
+  auth = "Bearer "+localStorage.getItem('access_token');
+  addHeader = new Headers();
+  addHeader.append("Authorization", auth);
+  
+  fetch("http://127.0.0.1:5000//Check_Token", {
+    headers: addHeader,
+  })
+  .then((response)=>response.text())
+  .then((text) => alert(text));
+}
+
+
+function Simple_Login(){
+  var id = document.querySelector('#id').value;
+  var pw = document.querySelector('#pw').value;
+  var info = {
+    ID:id,
+    PW:pw
+  };
+  fetch("http://127.0.0.1:5000//Simple_Login", {
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+    },
+    body:JSON.stringify(info) // str형식으로 보냄
+  })
+  .then((response)=>response.json())
+  .then(function(data){
+    for(var key in data){
+      localStorage.setItem(key, data[key]); 
+    }
+  })
+}
+
+function Logout(){
+  localStorage.clear()
+}
